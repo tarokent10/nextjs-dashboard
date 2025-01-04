106b2011977da4670770c3252304dcc1d6d92a93
tarokent <t.8.p.1.dogg@gmail.com>
2025-01-04 17:19:18 +0900

    mutation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -29,6 +29,35 @@ export async function createInvoice(formData: FormData) {
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `;
   // データが増えたので再検証が必要と理解している（が消しても変わらなかった。。） https://nextjs.org/docs/app/building-your-application/caching#data-cache
-  //   revalidatePath("/dashboard/invoices");
+  revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+// ...
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices"); // 再検証しないとページがリフレッシュされなかった！
 }
